ce9bd469c3f285815c50119430f8ff013d4df236
Robert Culbert <rfculbert@gmail.com>
2022-12-17 23:36:59 -0600

    final commit

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -6,7 +6,7 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-    driver.get('http://localhost:3000/')
+    driver.get('http://3.145.118.19')
 })
 
 afterAll(async () => {
@@ -15,6 +15,18 @@ afterAll(async () => {
 
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('Draw button shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('draw'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
+
+test('See all bots button shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('see-all'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
